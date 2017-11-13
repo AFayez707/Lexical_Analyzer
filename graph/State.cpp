b66be5e3c5d5c36b2e4e93411f4e8874b1fb2d37
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-11-13 18:35:48 +0200

    Graph pkg implementation added. (#1)
    Graph pkg implementation added.

---
+++ graph/State.cpp
@@ -0,0 +1,40 @@
+//
+// Created by ahmed on 11/13/17.
+//
+
+#include "State.h"
+
+int State::state_count = 1;
+
+State::State() {
+    this->state_name = state_count++;
+}
+
+vector<Edge> *State::get_out_edges() {
+    return &children;
+}
+
+void State::add_child(State *child, string weight) {
+    Edge edge(this, child, std::move(weight));
+    children.push_back(edge);
+}
+
+void State::set_accept_state(bool is_accept_state) {
+    this->accept_state = is_accept_state;
+}
+
+bool State::is_accept_state() {
+    return this->accept_state;
+}
+
+void State::set_token_type(string token_type) {
+    this->token_type = std::move(token_type);
+}
+
+string State::get_token_type() {
+    return token_type;
+}
+
+int State::get_state_name() {
+    return this->state_name;
+}
