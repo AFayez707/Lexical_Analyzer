a05b69ade2b51040073c09f28dbfd1d5bd6ebd8e
Abdelrahman <abdoumohamed96@gmail.com>
2017-11-14 20:00:57 +0200

    initial implementation of IDGenerator

---
+++ regex-to-nfa/IDGenerator.h
@@ -0,0 +1,16 @@
+//
+// Created by abdelrahman on 14/11/17.
+//
+
+#ifndef LEXICAL_ANALYZER_IDGENERATOR_H
+#define LEXICAL_ANALYZER_IDGENERATOR_H
+
+
+
+class IDGenerator {
+
+};
+
+
+
+#endif //LEXICAL_ANALYZER_IDGENERATOR_H
