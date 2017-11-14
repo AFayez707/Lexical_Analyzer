b53d17d3641c6356ed9df21a5251522cb12b59a3
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-11-14 23:48:09 +0200

    Added test pkg to gitignore, fix CMakeLists.txt.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    cout << "Hello World!" << endl;
-    return 0;
-}
+// TODO : To be implemented
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    cout << "Hello World!" << endl;
+//    return 0;
+//}
