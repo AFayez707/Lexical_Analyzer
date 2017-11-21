3f860acfc14a167367c7b92f58869ee226a573a7
Abdelrahman <abdoumohamed96@gmail.com>
2017-11-21 06:44:44 +0200

    add code for reading the source_code file

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include "regex-to-nfa/NFA.h"
 #include "regex-to-nfa/construct_NFA.h"
 #include "dfa_reduction/DFA_Reducer.h"
-#include "DFA_Generator/DFA_Generator.h"
+#include "dfa_generator/DFA_Generator.h"
 #include "regex-to-nfa/Regex.h"
 
 
@@ -28,12 +28,28 @@ int main() {
 
     DFA language_dfa(language_nfa, language);
     language_dfa.display();
-    Graph *g = language_dfa.as_graph();
 
-    DFA_Reducer reducer(g, language);
-    reducer.print();
-    reducer.minimize();
-    reducer.print();
+
+    freopen("source.txt", "r", stdin);
+    string source_code;
+
+    while (1) {
+        int c = getchar();
+
+        if(c == EOF)
+            break;
+
+        source_code.push_back((char) c);
+    }
+
+    language_dfa.simulate(source_code);
+
+
+//    Graph *g = language_dfa.as_graph();
+//    DFA_Reducer reducer(g, language);
+//    reducer.print();
+//    reducer.minimize();
+//    reducer.print();
 
     return 0;
 }
