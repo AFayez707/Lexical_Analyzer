8e2f1d75070de1be9c4577ff2c039e3f8d748598
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-12-18 14:55:07 +0200

    Project run from main.

--- main.cpp
+++ main.cpp
@@ -3,33 +3,35 @@
 //
 #include "parser/Parser.h"
 #include <iostream>
-#include <queue>
 
 using namespace std;
 
 int main(int argc, char **argv) {
     queue<string> tokens;
 
-    if (argc == 1) {
-        string line;
-        while (getline(cin, line)) {
-            string token = line.substr(0, line.find_first_of(' '));
-            if (token == "lexical_error:") {
-                cout << "Warning, lex reported an error => " << line.c_str() << endl;
-            } else {
-                tokens.push(token);
-                cout << "Read: " << token << endl;
-            }
-        }
-    } else if (argc == 3) {
-        // TODO: to be written
-    } else {
-        cout << "Invalid number of params, parse takes zero or two params.\n\n"
-                "pipe lex out put using -> ./lex | parse\n\n"
-                "            two params -> ./parse [java_cfg.txt file path] [tokens file path]\n\n";
-        exit(1);
-    }
+    ifstream in("tokens.txt");
+    string token;
+    while (getline(in, token)) tokens.push(token);
 
+//    if (argc == 1) {
+//        string line;
+//        while (getline(cin, line)) {
+//            string token = line.substr(0, line.find_first_of(' '));
+//            if (token == "lexical_error:") {
+//                cout << "Warning, lex reported an error => " << line.c_str() << endl;
+//            } else {
+//                tokens.push(token);
+//                cout << "" << token << endl;
+//            }
+//        }
+//    } else if (argc == 3) {
+//        // TODO: to be written
+//    } else {
+//        cout << "Invalid number of params, parse takes zero or two params.\n\n"
+//                "pipe lex out put using -> ./lex | ./parse\n\n"
+//                "            two params -> ./parse [java_cfg.txt file path] [tokens file path]\n\n";
+//        exit(1);
+//    }
 
     cout << "\n\n\nParsing:\n";
     Parser parser("java_cfg.txt", "parser-log.txt", tokens);
