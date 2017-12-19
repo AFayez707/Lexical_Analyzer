491cd9d4ffe4837897787c83c6a80be93b562f04
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-12-19 10:39:43 +0200

    Fuck My Life 2.

--- main.cpp
+++ main.cpp
@@ -9,30 +9,29 @@ using namespace std;
 int main(int argc, char **argv) {
     queue<string> tokens;
 
-    ifstream in("tokens.txt");
-    string token;
-    while (getline(in, token)) tokens.push(token);
-    tokens.push(DOLLAR_SIGN);
+//    ifstream in("tokens.txt");
+//    string token;
+//    while (getline(in, token)) tokens.push(token);
+//    tokens.push(DOLLAR_SIGN);
 
-//    if (argc == 1) {
-//        string line;
-//        while (getline(cin, line)) {
-//            string token = line.substr(0, line.find_first_of(' '));
-//            if (token == "lexical_error:") {
-//                cout << "Warning, lex reported an error => " << line.c_str() << endl;
-//            } else {
-//                tokens.push(token);
-//                cout << "" << token << endl;
-//            }
-//        }
-//    } else if (argc == 3) {
-//        // TODO: to be written
-//    } else {
-//        cout << "Invalid number of params, parse takes zero or two params.\n\n"
-//                "pipe lex out put using -> ./lex | ./parse\n\n"
-//                "            two params -> ./parse [java_cfg.txt file path] [tokens file path]\n\n";
-//        exit(1);
-//    }
+    if (argc == 1) {
+        string line;
+        while (getline(cin, line)) {
+            string token = line.substr(0, line.find_first_of(' '));
+            if (token == "lexical_error:") {
+                cout << "Warning, lex reported an error => " << line.c_str() << endl;
+            } else {
+                tokens.push(token);
+                cout << "" << token << endl;
+            }
+        }
+    } else {
+        cout << "Invalid number of params, parse takes zero or two params.\n\n"
+                "pipe lex out put using -> ./lex | ./parse\n\n";
+        exit(1);
+    }
+
+    tokens.push(DOLLAR_SIGN);
 
     Parser parser("java_cfg.txt", "parser-log.txt", tokens);
     parser.parse();
